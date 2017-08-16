e098758d9d6c1cf4ee4e3b90e9b5bf323296b3ee
oskar <oskar@outlandish.com>
2017-08-16 13:45:44 +0100

    Now a functioning React application with basic Home and About pages

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: {
+    app: "./js/app.js",
+    html: "./index.html",
+  },
+
+  context: __dirname + "/app",
+
+  output: {
+    filename: "[name].js",
+    path: __dirname + "/dist",
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"]
+      },
+      {
+        test: /\.html$/,
+        loader: "file-loader?name=[name].[ext]",
+      }
+    ]
+  }
+};
